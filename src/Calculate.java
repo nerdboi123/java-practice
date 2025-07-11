e6e5c506641aa36567973d5235afa613cf4ec285
nerdboi123 <133088931+nerdboi123@users.noreply.github.com>
2025-07-11 14:00:20 +0900

    feat: 계산기 객체지향 코딩 연습 수정하였습니다

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,20 +1,41 @@
 public class Calculate {
 
     //덧셈
-    public void
+    public int plus(int a, int b){
+        return a+b;
+    }
 
 
     //뺄셈
-
+    public int minus(int a, int b) {
+        return a-b;
+    }
 
 
     //곱셈
+    public int times(int a, int b) {
+        return a*b;
+    }
 
 
     //나눗셈
+    public int divide(int a, int b) {
+        return a/b;
+    }
 
-    //main
 
+    //main 계산기 작동 확인
+    public static void main(String[] args){
+        Calculate cal = new Calculate();
+
+        int a = 4;
+        int b = 2;
+        System.out.println("입력 받은 값: " +a+ ","+ b);
+        System.out.println("더하기: " + cal.plus(a,b));
+        System.out.println("빼기: " + cal.minus(a,b));
+        System.out.println("곱하기: " + cal.times(a,b));
+        System.out.println("나누기: " + cal.divide(a,b));
+    }
 
 
 
